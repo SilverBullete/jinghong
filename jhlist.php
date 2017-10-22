88f713c73158da20ee629ca4ee107b3c99472356
SilverBullete <32236496+SilverBullete@users.noreply.github.com>
2017-10-22 11:12:56 +0800

    Update jhlist.php

--- jhlist.php
+++ jhlist.php
@@ -68,68 +68,7 @@ else
 }
 
 
-/*$result = $conn->query($query);
-$num_results = $result->num_rows;
-for ($i=0;$i<$num_results;$i++)
-{
-    $row = $result->fetch_assoc();
 
-    if ($row['name'] == $name || $row['id'] == $id)
-    {
-        if ($row['name'] == $name && $row['id'] == 0)
-        {
-            echo '姓名：' . $row['name'];
-            echo '</br>学号：' . $row['id'];
-            echo '</br>专业：' . $row['major'];
-            echo '</br>学院：' . $row['academy'];
-            echo '</br>班级：' . $row['class'];
-            echo '</br>长号：' . $row['phone1'];
-            echo '</br>短号：' . $row['phone2'];
-            echo '</br>Git名字：' . $row['github_name'];
-            echo '</br>QQ：' . $row['qq'];
-            echo '</br>E-mail：' . $row['mail'];
-            exit;
-        }
-        elseif ($row['name'] == $name && $row['id'] == $id)
-        {
-            echo '姓名：' . $row['name'];
-            echo '</br>学号：' . $row['id'];
-            echo '</br>专业：' . $row['major'];
-            echo '</br>学院：' . $row['academy'];
-            echo '</br>班级：' . $row['class'];
-            echo '</br>长号：' . $row['phone1'];
-            echo '</br>短号：' . $row['phone2'];
-            echo '</br>Git名字：' . $row['github_name'];
-            echo '</br>QQ：' . $row['qq'];
-            echo '</br>E-mail：' . $row['mail'];
-            exit;
-        }
-        elseif ($row['name'] == '' && $row['id'] == $id)
-        {
-            echo '姓名：' . $row['name'];
-            echo '</br>学号：' . $row['id'];
-            echo '</br>专业：' . $row['major'];
-            echo '</br>学院：' . $row['academy'];
-            echo '</br>班级：' . $row['class'];
-            echo '</br>长号：' . $row['phone1'];
-            echo '</br>短号：' . $row['phone2'];
-            echo '</br>Git名字：' . $row['github_name'];
-            echo '</br>QQ：' . $row['qq'];
-            echo '</br>E-mail：' . $row['mail'];
-            exit;
-        }
-        else
-        {
-            echo "姓名与学号不匹配";
-            exit;
-        }
-    }
-    elseif($row['name'] !== $name || $row['id'] !== $id)
-    {
-        echo '所查询的姓名或学号不存在';
-        exit;
-    }
-}*/
 
 /**
  * Created by PhpStorm.
